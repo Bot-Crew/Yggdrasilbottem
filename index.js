7570f76da39e652680d479e6c9839e581d7bc006
PitGamer1909 <70101281+PitGamer1909@users.noreply.github.com>
2023-09-10 18:48:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,37 +4,37 @@ const { Client } = require('whatsapp-web.js');
 const client = new Client();
 
 client.on('qr', (qr) => {
-    console.log('QRCode Wurde dir Gescickt', qrcode.generate(qr, { small: true }));
+    console.log('QRCode został wysłany do Ciebie', qrcode.generate(qr, { small: true }));
 });
 client.on('ready', () => {
-    console.log('Yggdrasilbot ist nun Online');
+    console.log('Yggdrasilbot jest teraz online');
 });
-client.on('message', async(msg) => {
+client.on('message', async (msg) => {
     if (msg.body === 'Bot') {
         const chat = await msg.getChat();
         chat.sendStateRecording();
-        msg.reply('Yo Bruder ich bin der Yggdrasilbot. Gemacht von _Cypis_');
+        msg.reply('Yo brat, jestem Yggdrasilbot, stworzony przez _Cypis_.');
     } else if (msg.body === '!ping') {
         const chat = await msg.getChat();
-        let ping = 'Keine Ahnung habe ich nicht geschafft. :('
+        let ping = 'Nie mam pojęcia, nie udało mi się. :('
         chat.sendStateTyping();
-        msg.reply(`Boar Bruder das ist Anstrengender als nach Valhall zu laufen (schwitz)
+        msg.reply(`Kurczę, bracie, to jest trudniejsze niż chodzenie do Valhalli (spociłem się)
 ${ping}`);
     } else if (msg.body === '!besitzer') {
         const contact = await msg.getContact();
         const chat = await msg.getChat();
         const info = await client.info;
         chat.sendStateTyping();
-        msg.reply(`Boar, digga Cypis ist mein Besitzer!
-Trotzdem Grüße von ${info.pushname}.
-Wenn du _Cypis_ Kontaktieren willst schreibe = !kontakt `);
+        msg.reply(`Kurczę, digga Cypis jest moim właścicielem!
+Mimo to pozdrowienia od ${info.pushname}.
+Jeśli chcesz skontaktować się z _Cypis_, napisz !kontakt.`);
         //Cypis
     } else if (msg.body === '!kontakt') {
         let Cypis = '14427862189'
         const contact = await msg.getContact();
-        client.sendMessage(msg.from, `Hallo @${contact.number},
-Ich weiß weshalb du mich angeschrieben hast, aber hier: https://wa.me/${Cypis}.
-Menü Designer ist aus dem Team geflogen. LG Yggdrasilbot`, {
+        client.sendMessage(msg.from, `Cześć @${contact.number},
+Wiem, dlaczego do mnie napisałeś, ale tutaj jest link: https://wa.me/${Cypis}.
+Projektant menu został wyrzucony z zespołu. Pozdrowienia od Yggdrasilbota`, {
             mentions: [contact]
         });
 
@@ -44,21 +44,21 @@ Menü Designer ist aus dem Team geflogen. LG Yggdrasilbot`, {
             let newSubject = msg.body.slice(8);
             chat.setSubject(newSubject);
             chat.sendStateTyping();
-            msg.reply(`Beschreibung der Gruppe ${chat.name} wurde nun geändert zu ${newSubject}`);
+            msg.reply(`Opis grupy ${chat.name} został zmieniony na ${newSubject}`);
         } else {
-            msg.reply('Digga, funktioniert nur in Gruppen!');
+            msg.reply('Digga, to działa tylko w grupach!');
         }
     } else if (msg.body.startsWith('!sag ')) {
         const chat = await msg.getChat();
         chat.sendStateTyping();
         client.sendMessage(msg.from, msg.body.slice(5));
-        //du schreibst dir selbst!
+        //piszesz do siebie!
     } else if (msg.body.startsWith('!sendemir ')) {
         const contact = await msg.getContact();
         const chat = await msg.getChat
         let text = msg.body.slice(10);
         let nummer = `${contact.number}@c.us`
-        msg.reply(`*@${contact.number}* Du schriebst dir selbst:
+        msg.reply(`*@${contact.number}* Piszesz do siebie:
 ${text}`, nummer, {
             mentions: [contact]
         });
@@ -68,101 +68,97 @@ ${text}`, nummer, {
             let newDescription = msg.body.slice(15);
             chat.setDescription(newDescription);
             chat.sendStateTyping();
-            msg.reply(`Beschreibung von der Gruppe ${chat.name} wurde nun geändert zu ${newDescription}`);
+            msg.reply(`Opis grupy ${chat.name} został zmieniony na ${newDescription}`);
         } else {
-            msg.reply('Digga, funktioniert nur in Grupppen!');
+            msg.reply('Digga, to działa tylko w grupach!');
         }
     } else if (msg.body === '!verlass') {
-        // Leave the group
+        // Opuść grupę
         let chat = await msg.getChat();
         if (chat.isGroup) {
             chat.leave();
         } else {
-            msg.reply('Dieser Befehl kann nur in einer Gruppe verwendet werden!');
+            msg.reply('To polecenie można używać tylko w grupie!');
         }
     } else if (msg.body.startsWith('!blockier')) {
         const contact = await msg.getContact();
-        msg.reply('Bruder das tut mir grade leid in meinem Herzen. :<')
+        msg.reply('Bracie, mi przykro w moim sercu. :<')
         await contact.block();
     } else if (msg.body.startsWith('!kick')) {
         const contact = await msg.getContact();
         const chat = await msg.getChat();
         chat.sendStateTyping();
-        msg.reply(`Witam, Bruder denkst du ich habe genug wissen? Tut mir leid aber da irrst du dich :(`, );
+        msg.reply(`Witaj, bracie, myślisz, że mam wystarczająco wiedzy? Przykro mi, ale się mylisz :(`, );
     } else if (msg.body.startsWith('!entblockier')) {
         const contact = await msg.getContact();
         await contact.unblock();
-
     } else if (msg.body.startsWith('!betrete ')) {
         const inviteCode = msg.body.split('https://chat.whatsapp.com/')[1];
         const chat = await msg.getChat();
         try {
             await client.acceptInvite(inviteCode);
-            msg.reply('Gruppe erfolgreich Beigetreten!');
+            msg.reply('Pomyślnie dołączyłeś do grupy!');
         } catch (e) {
-            msg.reply('Der Einladungscode ist wahrscheinlich nicht mehr gültig.');
+            msg.reply('Kod zaproszenia jest prawdopodobnie nieaktualny.');
         }
-
     } else if (msg.body === '!gruppeninfo') {
         let chat = await msg.getChat();
         if (chat.isGroup) {
             chat.sendStateTyping();
             msg.reply(`
-*Gruppen Details*
-Name: ${chat.name}
-Beschreibung: ${chat.description}
-Erstellt am: ${chat.createdAt.toString()}
-Erstellt von: ${chat.owner.user}
-Benutzer: ${chat.participants.length}
+*Szczegóły grupy*
+Nazwa: ${chat.name}
+Opis: ${chat.description}
+Utworzona: ${chat.createdAt.toString()}
+Utworzona przez: ${chat.owner.user}
+Uczestnicy: ${chat.participants.length}
 Link: ${chat.link}
         `);
 
         } else {
-            msg.reply('Dieser Befehl kann nur in einer Gruppe verwendet werden!');
+            msg.reply('To polecenie można używać tylko w grupie!');
         }
     } else if (msg.body === '!gruppenbeschreibung') {
         let chat = await msg.getChat();
         if (chat.isGroup) {
             chat.sendStateTyping();
             msg.reply(`
-            Gruppenbeschreibung: 
+            Opis grupy: 
             ${chat.description}
             `);
         } else {
-            msg.reply('Dieser Befehl kann nur in einer Gruppe verwendet werden!');
+            msg.reply('To polecenie można używać tylko w grupie!');
         }
     } else if (msg.body === '!chats') {
         const chats = await client.getChats();
         const chat = await msg.getChat();
         chat.sendStateTyping();
-        client.sendMessage(msg.from, `Der Bot hat ${chats.length} offene chats.`);
-
+        client.sendMessage(msg.from, `Bot ma ${chats.length} otwartych rozmów.`);
     } else if (msg.body === '!info') {
         let info = client.info;
         const chat = await msg.getChat();
         const contact = msg.getContact();
         chat.sendStateTyping();
         client.sendMessage(msg.from, `
-*Verbindungsinfo*
-Bot Name: ${info.pushname}
-Bot Nummer: ${info.wid.user}
-Plattform: ${info.platform}
-Documents: ${info.filename}
+*Informacje o połączeniu*
+Nazwa Bota: ${info.pushname}
+Numer Bota: ${info.wid.user}
+Platforma: ${info.platform}
+Dokumenty: ${info.filename}
 `);
-
     } else if (msg.body.startsWith('!status ')) {
         const newStatus = msg.body.slice(8);
         const chat = await msg.getChat();
         await client.setStatus(newStatus);
         chat.sendStateTyping();
-        msg.reply(`Info wurde zu *${newStatus}* geupdated!`);
+        msg.reply(`Informacje zostały zaktualizowane na *${newStatus}*!`);
     } else if (msg.body.startsWith('!name ')) {
         const newName = msg.body.split(' ')[1];
         const chat = await msg.getChat();
         let info = client.info;
         await client.eventNames(newName);
         chat.sendStateTyping();
-        msg.reply(`Name wurde Aktualisiert auf *${newName}*`);
+        msg.reply(`Nazwa została zaktualizowana na *${newName}*`);
     } else if (msg.body === '!ich') {
         const contact = await msg.getContact();
         const chat = await msg.getChat();
@@ -181,7 +177,7 @@ Documents: ${info.filename}
         const contact = await msg.getContact();
         const chat = await msg.getChat();
         chat.sendStateTyping();
-        msg.reply(`Jo ich geb dir sofort Link. Moment ich gucke... : https://chat.whatsapp.com/KSejce3I2s3AGgpG2R7Uu8`);
+        msg.reply(`Spoko, zaraz dostaniesz link. Chwila, sprawdzam... : https://chat.whatsapp.com/KSejce3I2s3AGgpG2R7Uu8`);
     } else if (msg.body === '!löschen') {
         if (msg.hasQuotedMsg) {
             const chat = await msg.getChat();
@@ -190,7 +186,7 @@ Documents: ${info.filename}
                 quotedMsg.delete(true);
             } else {
                 chat.sendStateTyping();
-                msg.reply('Ich kann nur meine Nachrichten löschen!');
+                msg.reply('Mogę usuwać tylko moje wiadomości!');
             }
         }
     } else if (msg.body === '!stumm') {
@@ -198,144 +194,140 @@ Documents: ${info.filename}
         const unmuteDate = new Date();
         unmuteDate.setMinutes(unmuteDate.getMinutes() + 2);
         chat.sendStateTyping();
-        msg.reply('Dieser Chat wurde für 2 Minuten auf dem Gerät von dem Bot Stummgeschaltet');
+        msg.reply('Ta rozmowa została wyciszona na 2 minuty na urządzeniu bota');
         await chat.mute(unmuteDate);
     } else if (msg.body === '!stummh1') {
         const chat = await msg.getChat();
         const unmuteDate = new Date();
-        unmuteDate.getHours(unmuteDate.getHours() + 3);
+        unmuteDate.setHours(unmuteDate.getHours() + 3);
         chat.sendStateTyping();
-        msg.reply('Dieser Chat wurde für 3 Stunden auf dem Gerät von dem Bot Stummgeschaltet');
+        msg.reply('Ta rozmowa została wyciszona na 3 godziny na urządzeniu bota');
         await chat.mute(unmuteDate);
     } else if (msg.body === '!stummh2') {
         const chat = await msg.getChat();
         const unmuteDate = new Date();
-        unmuteDate.getHours(unmuteDate.getHours() + 6);
+        unmuteDate.setHours(unmuteDate.getHours() + 6);
         chat.sendStateTyping();
-        msg.reply('Dieser Chat wurde für 6 Stunden auf dem Gerät von dem Bot Stummgeschaltet');
+        msg.reply('Ta rozmowa została wyciszona na 6 godzin na urządzeniu bota');
         await chat.mute(unmuteDate);
     } else if (msg.body === '!stummh3') {
         const chat = await msg.getChat();
         const unmuteDate = new Date();
-        unmuteDate.getHours(unmuteDate.getHours() + 9);
+        unmuteDate.setHours(unmuteDate.getHours() + 9);
         chat.sendStateTyping();
-        msg.reply('Dieser Chat wurde für 9 Stunden auf dem Gerät von dem Bot Stummgeschaltet');
+        msg.reply('Ta rozmowa została wyciszona na 9 godzin na urządzeniu bota');
         await chat.mute(unmuteDate);
     } else if (msg.body === '!stummh4') {
         const chat = await msg.getChat();
         const unmuteDate = new Date();
-        unmuteDate.getHours(unmuteDate.getHours() + 12);
+        unmuteDate.setHours(unmuteDate.getHours() + 12);
         chat.sendStateTyping();
-        msg.reply('Dieser Chat wurde für 12 Stunden auf dem Gerät von dem Bot Stummgeschaltet');
+        msg.reply('Ta rozmowa została wyciszona na 12 godzin na urządzeniu bota');
         await chat.mute(unmuteDate);
     } else if (msg.body === '!stummh5') {
         const chat = await msg.getChat();
         const unmuteDate = new Date();
-        unmuteDate.getHours(unmuteDate.getHours() + 15);
+        unmuteDate.setHours(unmuteDate.getHours() + 15);
         chat.sendStateTyping();
-        msg.reply('Dieser Chat wurde für 15 Stunden auf dem Gerät von dem Bot Stummgeschaltet');
+        msg.reply('Ta rozmowa została wyciszona na 15 godzin na urządzeniu bota');
         await chat.mute(unmuteDate);
     } else if (msg.body === '!stummh6') {
         const chat = await msg.getChat();
         const unmuteDate = new Date();
-        unmuteDate.getHours(unmuteDate.getHours() + 18);
+        unmuteDate.setHours(unmuteDate.getHours() + 18);
         chat.sendStateTyping();
-        msg.reply('Dieser Chat wurde für 18 Stunden auf dem Gerät von dem Bot Stummgeschaltet');
+        msg.reply('Ta rozmowa została wyciszona na 18 godzin na urządzeniu bota');
         await chat.mute(unmuteDate);
     } else if (msg.body === '!stummh7') {
         const chat = await msg.getChat();
         const unmuteDate = new Date();
-        unmuteDate.getHours(unmuteDate.getHours() + 21);
+        unmuteDate.setHours(unmuteDate.getHours() + 21);
         chat.sendStateTyping();
-        msg.reply('Dieser Chat wurde für 21 Stunden auf dem Gerät von dem Bot Stummgeschaltet');
+        msg.reply('Ta rozmowa została wyciszona na 21 godzin na urządzeniu bota');
         await chat.mute(unmuteDate);
     } else if (msg.body === '!stummh8') {
         const chat = await msg.getChat();
         const unmuteDate = new Date();
-        unmuteDate.getHours(unmuteDate.getHours() + 24);
+        unmuteDate.setHours(unmuteDate.getHours() + 24);
         chat.sendStateTyping();
-        msg.reply('Dieser Chat wurde für 24 Stunden auf dem Gerät von dem Bot Stummgeschaltet');
+        msg.reply('Ta rozmowa została wyciszona na 24 godziny na urządzeniu bota');
         await chat.mute(unmuteDate);
     } else if (msg.body === '!stummd') {
         const chat = await msg.getChat();
         const unmuteDate = new Date();
-        unmuteDate.getDay(unmuteDate.getDay() + 666);
+        unmuteDate.setDate(unmuteDate.getDate() + 666);
         chat.sendStateTyping();
-        msg.reply('Dieser Chat wurde für 666 Tagen auf dem Gerät von dem Bot Stummgeschaltet');
+        msg.reply('Ta rozmowa została wyciszona na 666 dni na urządzeniu bota');
         await chat.mute(unmuteDate);
     } else if (msg.body === '!hilfe') {
         let info = client.info;
         msg.reply(`
-------[Yggdrasilbot Befehle]------
+------[Komendy Yggdrasilbota]------
 
 ~~~~~~~~~~~~~~~~~~~~~~
-BotName: ${info.pushname}
+Nazwa Bota: ${info.pushname}
 
-Wilkommen Beim Baum des 
-Lebens Yggdrasilbot.
+Witaj w Yggdrasilbocie - Drzewie Życia.
 
 ~~~~~~~~~~~~~~~~~~~~~~
 
 1. *!ping*
-Frag nach dem Ping von dem Bot.
-Benutzung: !ping
-        
+Zapytaj o ping bota.
+Użycie: !ping
+
 2. *!sag*
-Lass den Bot einen Gewünschten Text schreiben. 
-Benutzung: !sag HierKommtDeinText
-        
+Pozwól botowi napisać określony tekst.
+Użycie: !sag TuTwójTekst
+
 3. *!betrete*
-Bot Betritt deine Gruppe!
-Benutzung: https://chat.whatsapp.com/HierKommtDeinGruppenLink
-        
+Bot dołączy do twojej grupy!
+Użycie: https://chat.whatsapp.com/TutajTwójLinkDoGrupy
+
 4. *!gruppeninfo*
-Zeigt dir die Gruppeninfo
-Benutzung: !gruppeninfo
+Pokaż informacje o grupie.
+Użycie: !gruppeninfo
 
 5. *!gruppenbeschreibung*
-Zeigt dir die Gruppenbeschreibung
-Benutzung: !gruppenbeschreibung
+Pokaż opis grupy.
+Użycie: !gruppenbeschreibung
 
 6. *!chats*
-Ich zeige dir Wie viele Chats ich habe :)
-Benutzung: !chats 
+Pokaż, ile mam rozmów :)
+Użycie: !chats
 
 7. *!info*
-Du kannst meinen Namen uns so sehen
-Benutzung: !info
+Możesz zobaczyć moje dane kontaktowe
+Użycie: !info
 
 8. *!ich*
-Ich markiere dich
-Benutzung: !ich
+Oznaczam cię
+Użycie: !ich
 
 9. *!whatsapp*
-Ich schicke dir den Link zu deiner Nummer
-Benutzung: !whatsapp
+Przesyłam ci link do twojego numeru
+Użycie: !whatsapp
 
 10. *!löschen*
-Lässt mich meine Nachricht löschen
-Benutzung: !löschen
+Pozwala mi usuwać moje wiadomości!
+Użycie: !löschen
 
 11. *!besitzer*
-Zeigt dir mit welchen Befehl du _Cypis_ sprechen kannst.
-Beutzung: !besitzer
+Pokaż, jak możesz skontaktować się z _Cypis_.
+Użycie: !besitzer
 
 12. *!support*
-Gibt dir den Gruppenlink zur Supportgruppe von Yggdrasilbot.
-Benutzung: !support
+Podaję link do grupy wsparcia Yggdrasilbota.
+Użycie: !support
 
-_Made by Cypis_
+_Wykonane przez Cypisa_
 `);
-
-
     }
-
 });
 
-client.on('call', async(call, msg) => {
+client.on('call', async (call, msg) => {
     const contact = msg.getContact();
     if (!call.isGroup || !call.participants.length) {
-        client.sendText(call.participants, `Block wegen Anruf. Automatischer Block! Melde dich bei: https://wa.me/4917629368004/?text=Entblockier+mich+bitte!`);
+        client.sendText(call.participants, `Zablokowany z powodu połączenia. Automatyczne zablokowanie! Skontaktuj się tutaj: https://wa.me/4917629368004/?text=Odblokuj+mnie+proszę!`);
         await contact.block();
     }
 }, );
